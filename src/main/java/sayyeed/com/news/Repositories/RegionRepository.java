620d832a5f8dd99c635d9185e7a82284161015fb
Sayyeed <tmuhammadsayyid11@gmail.com>
2025-05-20 14:30:25 +0500

    RegionRepository added

---
+++ src/main/java/sayyeed/com/news/Repositories/RegionRepository.java
@@ -0,0 +1,8 @@
+package sayyeed.com.news.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sayyeed.com.news.Entities.RegionEntity;
+
+public interface RegionRepository extends CrudRepository<RegionEntity, Integer> {
+
+}
