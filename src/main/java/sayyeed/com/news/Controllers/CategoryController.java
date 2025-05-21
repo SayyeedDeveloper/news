164fa350273567a0900d13e3d03c0275e52c2579
Sayyeed <tmuhammadsayyid11@gmail.com>
2025-05-21 14:33:12 +0500

    Create Category API added

--- src/main/java/sayyeed/com/news/Controllers/CategoryController.java
+++ src/main/java/sayyeed/com/news/Controllers/CategoryController.java
@@ -1,8 +1,13 @@
 package sayyeed.com.news.Controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sayyeed.com.news.DTOs.CategoryDTO;
 import sayyeed.com.news.Services.CategoryService;
 
 @RestController
@@ -12,4 +17,8 @@ public class CategoryController {
     @Autowired
     private CategoryService service;
 
+    @PostMapping("")
+    public ResponseEntity<CategoryDTO> create(@Valid @RequestBody CategoryDTO dto){
+        return ResponseEntity.ok(service.create(dto));
+    }
 }
--- src/main/java/sayyeed/com/news/Services/CategoryService.java
+++ src/main/java/sayyeed/com/news/Services/CategoryService.java
@@ -2,7 +2,12 @@ package sayyeed.com.news.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sayyeed.com.news.DTOs.CategoryDTO;
+import sayyeed.com.news.Entities.CategoryEntity;
+import sayyeed.com.news.Exceptions.AppBadException;
 import sayyeed.com.news.Repositories.CategoryRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CategoryService {
@@ -10,4 +15,24 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    public CategoryDTO create(CategoryDTO dto){
+        try {
+            CategoryEntity entity = new CategoryEntity();
+            entity.setOrder_number(dto.getOrderNumber());
+            entity.setNameUz(dto.getNameUz());
+            entity.setNameRu(dto.getNameRu());
+            entity.setNameEn(dto.getNameEn());
+            entity.setCategoryKey(dto.getCategoryKey());
+            entity.setCreated_date(LocalDateTime.now());
+            repository.save(entity);
+            dto.setId(entity.getId());
+            dto.setCreatedDate(entity.getCreated_date());
+            return dto;
+        }catch (Exception e){
+            if(e.getMessage().contains("unique constraint") || e.getMessage().contains("Duplicate entry")){
+                throw new AppBadException("Region with order number " + dto.getOrderNumber() + " already exists");
+            }
+            throw e;
+        }
+    }
 }
