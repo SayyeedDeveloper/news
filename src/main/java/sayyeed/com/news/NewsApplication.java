20ca5c64cbdc15b56bd9d0a9dd5a0a861b184f5c
Sayyeed <tmuhammadsayyid11@gmail.com>
2025-05-29 17:31:38 +0500

    Auth setup

--- src/main/java/sayyeed/com/news/NewsApplication.java
+++ src/main/java/sayyeed/com/news/NewsApplication.java
@@ -1,10 +1,17 @@
 package sayyeed.com.news;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewsApplication {
+    static {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+        System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NewsApplication.class, args);
